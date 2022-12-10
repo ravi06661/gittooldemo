1033779b46cf762377cfa3d0081150ec4a5fd48f
ravi06661 <ravi066612@gmail.com>
2022-12-10 15:17:47 +0530

    new file added

---
+++ Vowels.java
@@ -0,0 +1,20 @@
+class Test
+{
+	public static void main(String[] args) {
+	
+	String s="dollop infotech";
+	int i,size=strlen(s);
+	for(i=0;i<size;i++)
+	{
+		if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u')
+
+		{
+			System.out.println(i);
+		}
+	}	
+   
+
+
+
+	}
+}
