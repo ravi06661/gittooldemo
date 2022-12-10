784bf7182b70b59cd5cfa0881a1ddf865459e9f1
ravi06661 <ravi066612@gmail.com>
2022-12-10 15:49:11 +0530

    new Table file added

---
+++ Table.java
@@ -0,0 +1,14 @@
+class Table
+{
+	public static void main(String[] args) {
+		int i,j,n=1;
+		for(i=1;i<=10;i++)
+		{
+			for(j=n;j<=10;j++)
+				System.out.print(j*i );
+			System.out.println();
+		}
+	      
+		System.out.println();
+	}
+}
